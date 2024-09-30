09165c75a2b69a0f8db0f886fbc7d2090e3ec222
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2024-09-30 10:06:02 +0200

    JavaScript afprøvning

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -2,4 +2,37 @@
  * file: js/js.js
  * purpose: Behaviors
  **/
-console.log('Success: JavaScript running! - hello you')
+console.log('Success: JavaScript running! - hello you');
+
+/* her er en kommentar */
+console.log("Hello World!!");
+
+// her er også en kommentar 
+console.log("Dav");
+
+// streng er bogstaver altså tekst - har anførselstegn omkring sig (anførelsestegn = "" og '')
+// tal har ikke anførelses tegn omkring sig
+
+/** variabler - streng */
+let a = "Skriv en besked bar fordi";
+
+
+// variabler - tal - JavaScript jan lave matematik
+let b = 23;
+let c = 50;
+console.log(b * b + c);
+
+//booleans - noget der er rigtigt eller forkert (true/false)
+let julemandenEksistere = true;
+console.log('Findes julemanden? ' + julemandenEksistere);
+
+let aftensmas = false;
+
+let vaer_23_booked = false;
+
+//kontrolstrukturer - if, else
+
+/**kontatinering
+ * når man sætter/klistre ting sammen med +, fx ved linje 27
+*/
+let header = "<h1>" + a + "</h1>";
