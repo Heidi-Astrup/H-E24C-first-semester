164a842cb7f18ea7c0796810d445290da422d256
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2024-10-01 11:49:10 +0200

    redigeringer

--- javascript-intro/js/js.js
+++ javascript-intro/js/js.js
@@ -10,12 +10,13 @@ console.log("Hello World!!");
 // her er også en kommentar 
 console.log("Dav");
 
+
 // streng er bogstaver altså tekst - har anførselstegn omkring sig (anførelsestegn = "" og '')
 // tal har ikke anførelses tegn omkring sig
 
-/** variabler - streng */
-let a = "Skriv en besked bar fordi";
 
+/** variabler - streng */
+let a = "Skriv en besked bare fordi";
 
 // variabler - tal - JavaScript jan lave matematik
 let b = 23;
@@ -36,3 +37,10 @@ let vaer_23_booked = false;
  * når man sætter/klistre ting sammen med +, fx ved linje 27
 */
 let header = "<h1>" + a + "</h1>";
+console.log(header);
+
+//brug JS til at ændre noget HTML
+document.getElementById("Hej").innerHTML = header;
+document.getElementById("Hej").style.color = "green";
+
+
